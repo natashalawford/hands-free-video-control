1d71c2c775fa5aa51e12ecb5b5c7fae6dfc73f64
natashalawford <natasha.lawford@gmail.com>
2025-05-07 17:29:34 -0400

    Added basic hand detection with MediaPipe

--- src/main.py
+++ src/main.py
@@ -1,6 +1,19 @@
 # Entry point to run the app
 
 import cv2
+import mediapipe as mp
+
+# Initialize MediaPipe Hands
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(
+                        static_image_mode=False, 
+                        max_num_hands=1, 
+                        min_detection_confidence=0.7,
+                        min_tracking_confidence=0.7
+                       )
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
 
 def run_webcam():
     # Open a connection to the webcam
@@ -16,6 +29,24 @@ def run_webcam():
         if not read:
             print("Error: frame is unreadable")
             break
+
+        #Convert the frame to RGB for MediaPipe processing
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
+        # Process the frame and detect a single hand
+        results = hands.process(frame_rgb)
+
+        # Draw hand landmarks on the frame if hand is detected
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    frame,
+                    hand_landmarks,
+                    mp_hands.HAND_CONNECTIONS,
+                    #Add colours to different parts of the hand:
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style()
+                )
 
         # Display the each frame:
         cv2.imshow('Hands Free Video Control - Webcam Feed', frame)
