cf19de3bc4f2bd18ba9d52ccf5f400094754dfb3
natashalawford <natasha.lawford@gmail.com>
2025-05-08 20:24:47 -0400

    Detect hand gesture play/pause + trigger cooldown

--- src/gesture_controller.py
+++ src/gesture_controller.py
@@ -1,1 +1,28 @@
 # Handles webcam: hand and face detection
+
+#CONSTANTS:
+
+# Mediapipe allocated finger coordinates
+THUMB_BASE = 3
+INDEX_BASE = 6
+MIDDLE_BASE = 10
+RING_BASE = 14
+PINKY_BASE = 18
+
+THUMB_TIP = 4
+INDEX_TIP = 8
+MIDDLE_TIP = 12
+RING_TIP = 16
+PINKY_TIP = 20
+
+def is_open_palm(hand_landmarks):
+    fingers_up = 0
+    finger_tips = [THUMB_TIP, INDEX_TIP, MIDDLE_TIP, RING_TIP, PINKY_TIP]
+    finger_bases = [THUMB_BASE, INDEX_BASE, MIDDLE_BASE, RING_BASE, PINKY_BASE]
+
+    for tip, base in zip(finger_tips, finger_bases):
+        # Check if the tip is above the base in y-coordinates
+        if hand_landmarks.landmark[tip].y < hand_landmarks.landmark[base].y:
+            fingers_up += 1
+
+    return fingers_up == 5
--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
 # Entry point to run the app
 
+import sys
+import time
 import cv2
 import mediapipe as mp
+import gesture_controller
+
+# CONSTANTS
+COOLDOWN_TIME = 2  # seconds between gestures, prevents multiple triggers
 
 # Initialize MediaPipe Hands
 mp_hands = mp.solutions.hands
@@ -18,6 +24,7 @@ mp_drawing_styles = mp.solutions.drawing_styles
 def run_webcam():
     # Open a connection to the webcam
     cap = cv2.VideoCapture(0)
+    last_time = 0  # last time a gesture was detected
 
     if not cap.isOpened():
         print("Error: Could not open webcam")
@@ -35,18 +42,28 @@ def run_webcam():
         
         # Process the frame and detect a single hand
         results = hands.process(frame_rgb)
-
+        
         # Draw hand landmarks on the frame if hand is detected
+        
+        current_time = int(time.time())
+
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(
                     frame,
                     hand_landmarks,
                     mp_hands.HAND_CONNECTIONS,
-                    #Add colours to different parts of the hand:
+                    # Add colours to different parts of the hand:
                     mp_drawing_styles.get_default_hand_landmarks_style(),
                     mp_drawing_styles.get_default_hand_connections_style()
                 )
+
+            if gesture_controller.is_open_palm(hand_landmarks) and \
+                (current_time - last_time) > COOLDOWN_TIME:
+                print("Open palm detected: triggering play/pause")
+                print(last_time)
+                # Trigger play/pause action here
+                last_time = current_time
 
         # Display the each frame:
         cv2.imshow('Hands Free Video Control - Webcam Feed', frame)
